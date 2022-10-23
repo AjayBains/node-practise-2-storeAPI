eb9c3b2651f378dcb3132b43781be9d6489cb636
Ajay_Bains <ajay.bains@codvo.ai>
2022-10-23 16:00:38 +0530

    sort added

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,38 @@
+const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("testing async error");
-  res.status(200).json({ msg: "products testing route" });
+  // throw new Error("testing async error");
+  // const products = await Product.find({
+  //   featured: true,
+  //   name: "a first wooden table",
+  // });
+  const products = await Product.find({}).sort("-name -price");
+  // res.status(200).json({ msg: "products testing route" });
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products  route" });
+  console.log(req.query);
+  const { featured, company, name, sort } = req.query;
+  queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    console.log("Sort", sort);
+    result.sort(sortList);
+  } else {
+    result.sort("createdAt");
+  }
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
