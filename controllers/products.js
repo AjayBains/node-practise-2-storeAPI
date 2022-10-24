a2265182cfed58063b0b807706891f491afe7d0e
Ajay_Bains <ajay.bains@codvo.ai>
2022-10-24 10:11:12 +0530

    numeric filters added

--- controllers/products.js
+++ controllers/products.js
@@ -5,13 +5,16 @@ const getAllProductsStatic = async (req, res) => {
   //   featured: true,
   //   name: "a first wooden table",
   // });
-  const products = await Product.find({}).sort("-name -price");
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("name -price")
+    .select("price name");
   // res.status(200).json({ msg: "products testing route" });
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
   console.log(req.query);
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+
   queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -21,6 +24,33 @@ const getAllProducts = async (req, res) => {
   }
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$eq",
+    };
+
+    const regEx = /\b(<|>|>=|=|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+
+    // console.log(filters);
+    //
+    // console.log(numericFilters);
+    console.log(queryObject);
   }
   let result = Product.find(queryObject);
   if (sort) {
